c365adbfc29de79d284463b69310c43bf7c830d8
Tien Do Nam <dev.tien.donam@gmail.com>
2020-11-14 20:20:47 +0100

    feat: add custom query

--- rest-r2dbc-flyway/src/main/kotlin/com/example/demo/database/Person.kt
+++ rest-r2dbc-flyway/src/main/kotlin/com/example/demo/database/Person.kt
@@ -1,6 +1,7 @@
 package com.example.demo.database
 
 import org.springframework.data.annotation.Id
+import org.springframework.data.r2dbc.repository.Query
 import org.springframework.data.relational.core.mapping.Table
 import org.springframework.data.repository.kotlin.CoroutineCrudRepository
 
@@ -16,4 +17,8 @@ interface PersonRepo: CoroutineCrudRepository<Person, Int> {
 
     // SELECT * FROM person WHERE age >= 42
     suspend fun findByAgeGreaterThanEqual(age: Int): List<Person>
+
+    // This is how to implement a custom query
+    @Query("SELECT * FROM person WHERE age = :age AND name LIKE '%a%'")
+    suspend fun findCustom(age: Int): List<Person>
 }
